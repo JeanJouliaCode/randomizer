65b26557f423ca66e971aac88b2feb8b80623622
Jean Joulia <jjeeaann2015@gmail.com>
2022-02-06 14:37:56 +0000

    homepage layout

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <Button label="key" @click="click" />
-    <TextArea />
+    <div class="header"><h1>Random wheel</h1></div>
+    <div class="main">
+      <div class="spiningWheel"></div>
+    </div>
+    <div class="right">
+      <select id="cars" name="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="fiat">Fiat</option>
+        <option value="audi">Audi</option>
+      </select>
+      <TextArea class="TextArea" />
+      <Button label="Spin the wheel" @click="click" />
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
@@ -22,3 +34,57 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  display: grid;
+  height: 100vh;
+  width: 100vw;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 17em 4fr 1fr;
+}
+
+.spiningWheel {
+  width: 57em;
+  height: 57em;
+
+  border-radius: 50%;
+  background: rgb(216, 115, 115);
+}
+
+.header {
+  grid-column: 1 / 3;
+  grid-row: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 3em;
+}
+.main {
+  grid-column: 1 / 2;
+  grid-row: 2;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.right {
+  grid-column: 2 / 3;
+  grid-row: 2;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.TextArea {
+  height: 37em;
+  width: 34em;
+  margin: 1em 0 1em 0;
+}
+
+.footer {
+  grid-column: 1 / 3;
+  grid-row: 3;
+}
+</style>
